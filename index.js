3333de80943b1f7bbf7cdc235f9dcc6ec9f9e5b0
Ilhamskhyi <98175032+Ilhamskhyi@users.noreply.github.com>
2022-01-25 00:07:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,110 @@
+"use strict";
+const {
+	default: makeWASocket,
+	BufferJSON,
+	initInMemoryKeyStore,
+	DisconnectReason,
+	AnyMessageContent,
+	useSingleFileAuthState,
+	delay
+} = require("@adiwajshing/baileys-md")
+const figlet = require("figlet");
+const fs = require("fs");
+const moment = require('moment')
+const chalk = require('chalk')
+const logg = require('pino')
+const clui = require('clui')
+const { Spinner } = clui
+const { serialize } = require("./lib/myfunc");
+const { color, mylog, infolog } = require("./lib/color");
+const time = moment(new Date()).format('HH:mm:ss DD/MM/YYYY')
+let setting = JSON.parse(fs.readFileSync('./config.json'));
+let session = `./${setting.sessionName}.json`
+const { state, saveState } = useSingleFileAuthState(session)
+
+function title() {
+      console.clear()
+	  console.log(chalk.bold.green(figlet.textSync('𝑪𝑨𝑻𝒁𝒀 - 𝑩𝑶𝑻 𝑴𝑫', {
+		font: 'Standard',
+		horizontalLayout: 'default',
+		verticalLayout: 'default',
+		width: 80,
+		whitespaceBreak: false
+	})))
+	console.log(chalk.yellow(`\n                        ${chalk.yellow('[ Created By ilham - skhyzi]')}\n\n${chalk.red('Catzy - Bot')} : ${chalk.white('WhatsApp Bot Multi Device')}\n${chalk.red('Follow Insta skhyzi')} : ${chalk.white('ilham - skhyzi')}\n${chalk.red('Message Me On WhatsApp')} : ${chalk.white('+687710621')}\n${chalk.red('Donate')} : ${chalk.white('687710621 ( Gopay/Pulsa )')}\n`))
+}
+
+/**
+* Uncache if there is file change;
+* @param {string} module Module name or path;
+* @param {function} cb <optional> ;
+*/
+function nocache(module, cb = () => { }) {
+	console.log(`Module ${module} sedang diperhatikan terhadap perubahan`) 
+	fs.watchFile(require.resolve(module), async () => {
+		await uncache(require.resolve(module))
+		cb(module)
+	})
+}
+/**
+* Uncache a module
+* @param {string} module Module name or path;
+*/
+function uncache(module = '.') {
+	return new Promise((resolve, reject) => {
+		try {
+			delete require.cache[require.resolve(module)]
+			resolve()
+		} catch (e) {
+			reject(e)
+		}
+	})
+}
+
+const status = new Spinner(chalk.cyan(` Booting WhatsApp Bot`))
+const starting = new Spinner(chalk.cyan(` Preparing After Connect`))
+const reconnect = new Spinner(chalk.redBright(` Reconnecting WhatsApp Bot`))
+
+const connectToWhatsApp = async () => {
+	const conn = makeWASocket({ printQRInTerminal: true, logger: logg({ level: 'fatal' }), auth: state })
+	title()
+	
+	/* Auto Update */
+	require('./message/help')
+	require('./lib/myfunc')
+	require('./message/msg')
+	nocache('./message/help', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+	nocache('./lib/myfunc', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+	nocache('./message/msg', module => console.log(chalk.greenBright('[ WHATSAPP BOT ]  ') + time + chalk.cyanBright(` "${module}" Telah diupdate!`)))
+	
+	conn.multi = true
+	conn.nopref = false
+	conn.prefa = '𝑪𝑨𝑻𝒁𝒀 - 𝑩𝑶𝑻 𝑴𝑫'
+	conn.ev.on('messages.upsert', async m => {
+		if (!m.messages) return;
+		var msg = m.messages[0]
+		msg = serialize(conn, msg)
+		msg.isBaileys = msg.key.id.startsWith('BAE5') || msg.key.id.startsWith('3EB0')
+		require('./message/msg')(conn, msg, m, setting)
+	})
+	conn.ev.on('connection.update', (update) => {
+		const { connection, lastDisconnect } = update
+		if (connection === 'close') {
+			status.stop()
+			reconnect.stop()
+			starting.stop()
+			console.log(mylog('Server Ready ✓'))
+			lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut 
+			? connectToWhatsApp()
+			: console.log(mylog('Wa web terlogout...'))
+		}
+	})
+	conn.ev.on('creds.update', () => saveState)
+	
+	conn.reply = (from, content, msg) => conn.sendMessage(from, { text: content }, { quoted: msg })
+
+	return conn
+}
+
+connectToWhatsApp()
+.catch(err => console.log(err))
